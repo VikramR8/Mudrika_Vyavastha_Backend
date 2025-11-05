865abd2f8593a0ccc2d6ee7cbf5b85b9a137a367
VikramR8 <vikramadityareshmi@gmail.com>
2025-11-05 18:14:28 +0530

    added excluded paths in JWTRequestFilter

--- src/main/java/in/vikramaditya/MudrikaVyavastha/security/JWTRequestFilter.java
+++ src/main/java/in/vikramaditya/MudrikaVyavastha/security/JWTRequestFilter.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -23,13 +24,30 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
     private final JWTUtil jwtUtil;
 
+    private static final List<String> EXCLUDED_PATHS = List.of(
+            "/register",
+            "/login",
+            "/activate",
+            "/status",
+            "/health"
+    );
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        if (EXCLUDED_PATHS.contains(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
         final String authHeader = request.getHeader("Authorization");
         String email = null;
         String jwt = null;
 
-        if(authHeader!=null && authHeader.startsWith("Bearer")) {
+        if(authHeader!=null && authHeader.startsWith("Bearer ")) {
             jwt = authHeader.substring(7);
             email = jwtUtil.extractEmail(jwt);
         }
