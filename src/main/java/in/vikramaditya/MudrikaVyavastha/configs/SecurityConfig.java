35238775ba60d5535d72dc7e6c25ba8dbc864920
VikramR8 <vikramadityareshmi@gmail.com>
2025-12-31 17:48:59 +0530

    Download and Email Incomes and Expenses

--- src/main/java/in/vikramaditya/MudrikaVyavastha/configs/SecurityConfig.java
+++ src/main/java/in/vikramaditya/MudrikaVyavastha/configs/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth.requestMatchers("/status", "/health", "/register", "/activate", "/login", "/keep-alive").permitAll()
+                .authorizeHttpRequests(auth->auth.requestMatchers("/status", "/health", "/register", "/activate", "/login", "/excel/download/**").permitAll()
                         .anyRequest().authenticated())
                         .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
